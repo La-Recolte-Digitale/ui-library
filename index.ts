9455421341283b1838e9a6c5097bbc1f92ea0f13
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2019-04-08 16:55:25 +0300

    started with library and demo

---
+++ index.ts
@@ -0,0 +1,2 @@
+export * from './src/la-recolte-action-bar/la-recolte-action-bar.component';
+export * from './src/app.module';
---
+++ src/la-recolte-action-bar/la-recolte-action-bar.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'la-recolte-action-bar',
+  template: `
+    <div class="buttons-bottom">
+      <ng-content></ng-content>
+    </div>
+    <div class="bottom-spacer"></div>
+  `,
+  styles: [`
+    .buttons-bottom {
+      white-space: nowrap;
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      z-index: 100;
+      display: flex;
+      background: white;
+    }
+
+    .buttons-bottom button {
+      display: flex;
+      flex: 1;
+      border-radius: 0 !important;
+    }
+
+    .buttons-bottom button:not(:first-child) {
+      border-left: 3px solid !important;
+    }
+
+    .bottom-spacer {
+      height: 60px;
+    }
+  `],
+  encapsulation: ViewEncapsulation.None
+})
+export class LaRecolteActionBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
