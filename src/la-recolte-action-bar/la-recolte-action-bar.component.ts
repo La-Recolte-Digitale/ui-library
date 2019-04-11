a9f8f061fac72a95ef6673bf248cf7193219bd72
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2019-04-11 19:29:48 +0300

    build fixes

--- src/la-recolte-action-bar/la-recolte-action-bar.component.ts
+++ src/la-recolte-action-bar/la-recolte-action-bar.component.ts
@@ -2,39 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'la-recolte-action-bar',
-  template: `
-    <div class="buttons-bottom" [style.position]="absolute ? 'absolute' : 'fixed'">
-      <ng-content></ng-content>
-    </div>
-    <div class="bottom-spacer"></div>
-  `,
-  styles: [`
-    .buttons-bottom {
-      white-space: nowrap;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      z-index: 100;
-      display: flex;
-      background: white;
-      z-index: 1;
-    }
-
-    .buttons-bottom >>> button {
-      display: flex;
-      flex: 1;
-      border-radius: 0 !important;
-    }
-
-    .buttons-bottom >>> button:not(:first-child) {
-      margin-left: 3px !important;
-      box-shadow: none !important;
-    }
-
-    .bottom-spacer {
-      height: 54px;
-    }
-  `]
+  templateUrl: './la-recolte-action-bar.component.html',
+  styleUrls: ['./la-recolte-action-bar.component.scss'],
 })
 export class LaRecolteActionBarComponent implements OnInit {
   @Input() absolute: boolean = false;
